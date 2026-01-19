cc1019e8c7d00338ebcd8f3661539124be4d4177
Daniellpego02 <pegopvp@gmail.com>
2026-01-18 22:10:30 -0300

    Change module export to ES6 default export

--- api/lowify.js
+++ api/lowify.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async (req, res) => {
   console.log('[Lowify] Request received:', { method: req.method, url: req.url });
   console.log('[Lowify] Request body:', req.body);
   
